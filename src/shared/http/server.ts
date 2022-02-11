1aab346c0a7147539aa2483cf110e6ca40132e4f
= <=>
2022-02-10 22:23:14 -0300

    feat: Initial commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,29 @@
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import cors from "cors";
+import routes from "./routes"
+import '@shared/typeorm';
+import CustomException from "@shared/exceptions/CustomException";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof CustomException) {
+        return response.status(error.statusCode).json({
+            status: error.statusCode,
+            message: error.message
+        });
+    }
+
+    return response.status(500).json({
+        status: 500,
+        message: "Internal Server Error"
+    })
+});
+
+app.listen(5000, () => {
+    console.log("🚀 SERVER START ON PORT 5000 🚀");
+})
